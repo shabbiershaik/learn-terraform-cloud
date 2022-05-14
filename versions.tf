eed00fbc04e78ddd2b30a43055a08a114c779825
Shabbier Shaik <shabbier@gmail.com>
2022-05-14 14:29:43 -0500

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "tfe_shabbier"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
